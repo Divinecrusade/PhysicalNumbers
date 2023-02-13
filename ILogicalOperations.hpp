62d7781bdb9b5034ff177b95a9d31bf3d0aae3ff
DivineCrusade <gorlovdima@inbox.ru>
2023-02-13 09:53:04 +0300

    feat: Add interface ILogicalOperations with some default realisations for logical operations

---
+++ ILogicalOperations.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace PhysicalNumbers
+{
+    template<class T>
+    __interface ILogicalOperations
+    {
+        static bool operator<(T const& lhs, T const& rhs) = 0;
+
+        static bool operator>(T const& lhs, T const& rhs) { return !(lhs < rhs); }
+
+        static bool operator!=(T const& lhs, T const& rhs) { return lhs < rhs || rhs < lhs; }
+        static bool operator==(T const& lhs, T const& rhs) { return !(lhs < rhs || rhs < lhs); }
+
+        static bool operator<=(T const& lhs, T const& rhs) { return lhs < rhs || lhs == rhs; }
+        static bool operator>=(T const& lhs, T const& rhs) { return lhs > rhs || lhs == rhs; }
+
+        static bool bool(T const& rhs) = 0;
+    };
+}
