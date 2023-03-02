9c27bfbf98e9b3a0e31385f0d952f0d63263f151
DivineCrusade <gorlovdima@inbox.ru>
2023-03-02 11:23:50 +0300

    refactor: Make type value of Quantity template

--- Quantity.hpp
+++ Quantity.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "MetaProgramming.hpp"
+#include "Dimension.hpp"
 
 namespace PhysicalNumbers
 {
     // Declaration
 
-    template<typename IntList>
+    template<typename IntList, class T = double>
     class Quantity
     {
 
@@ -14,147 +14,147 @@ namespace PhysicalNumbers
 
         using type = typename IntList;
 
-        explicit Quantity(double init_val = 0.);
-        Quantity(Quantity const& num);
-        Quantity<IntList>& operator=(Quantity<IntList> const& num);
+        explicit Quantity(T init_val);
+        Quantity(Quantity<IntList, T> const& num);
+        Quantity<IntList, T>& operator=(Quantity<IntList, T> const& num);
 
-        Quantity<IntList>& operator=(Quantity<IntList>&&) = default;
-        Quantity(Quantity&&) = default;
+        Quantity<IntList, T>& operator=(Quantity<IntList, T>&&) = default;
+        Quantity(Quantity<IntList, T>&&) = default;
 
         ~Quantity() = default;
 
-        double const& get_value() const;
-        double      & get_value();
+        T const& get_value() const;
+        T      & get_value();
 
-        Quantity<IntList>& operator-=(Quantity<IntList> const& rhs);
-        Quantity<IntList>& operator+=(Quantity<IntList> const& rhs);
-        Quantity<IntList>& operator*=(Quantity<IntList> const& rhs);
-        Quantity<IntList>& operator/=(Quantity<IntList> const& rhs);
+        Quantity<IntList, T>& operator-=(Quantity<IntList, T> const& rhs);
+        Quantity<IntList, T>& operator+=(Quantity<IntList, T> const& rhs);
+        Quantity<IntList, T>& operator*=(Quantity<IntList, T> const& rhs);
+        Quantity<IntList, T>& operator/=(Quantity<IntList, T> const& rhs);
 
     private:
 
-        double val{ };
+        T val{ };
     };
 
-    template<typename IntList>
-    bool operator<(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    bool operator<(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs);
 
-    template<typename IntList>
-    bool operator>(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    bool operator>(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs);
 
-    template<typename IntList>
-    bool operator==(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    bool operator==(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs);
 
-    template<typename IntList>
-    bool operator!=(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    bool operator!=(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs);
 
-    template<typename IntList>
-    bool operator<=(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    bool operator<=(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs);
 
-    template<typename IntList>
-    bool operator>=(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    bool operator>=(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs);
 
 
-    template<typename IntList>
-    Quantity<IntList> operator-(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    Quantity<IntList, T> operator-(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs);
 
-    template<typename IntList>
-    Quantity<IntList> operator+(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    Quantity<IntList, T> operator+(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs);
 
-    template<typename IntList1, typename IntList2>
-    Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Plus>> operator*(Quantity<IntList1> const& lhs, Quantity<IntList2> const& rhs);
+    template<typename IntList1, typename IntList2, class T>
+    Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Plus>, T> operator*(Quantity<IntList1, T> const& lhs, Quantity<IntList2, T> const& rhs);
 
-    template<typename IntList1, typename IntList2>
-    Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Minus>> operator/(Quantity<IntList1> const& lhs, Quantity<IntList2> const& rhs);
+    template<typename IntList1, typename IntList2, class T>
+    Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Minus>, T> operator/(Quantity<IntList1, T> const& lhs, Quantity<IntList2, T> const& rhs);
 
-    template<typename IntList>
-    Quantity<IntList> operator*(double lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    Quantity<IntList, T> operator*(double lhs, Quantity<IntList, T> const& rhs);
 
-    template<typename IntList>
-    Quantity<MetaProgramming::Zip<MetaProgramming::Generate<MetaProgramming::Length<IntList>::value>, IntList, MetaProgramming::Minus>>
-    operator/(double lhs, Quantity<IntList> const& rhs);
+    template<typename IntList, class T>
+    Quantity<MetaProgramming::Zip<MetaProgramming::Generate<MetaProgramming::Length<IntList>::value>, IntList, MetaProgramming::Minus>, T>
+    operator/(double lhs, Quantity<IntList, T> const& rhs);
 
 
     // Implementation
 
-    template<typename IntList>
-    Quantity<IntList>::Quantity(double init_val) : val(init_val) { }
+    template<typename IntList, class T>
+    Quantity<IntList, T>::Quantity(T init_val) : val(init_val) { }
 
-    template<typename IntList>
-    Quantity<IntList>::Quantity(Quantity const& num) : val(num.val) { }
+    template<typename IntList, class T>
+    Quantity<IntList, T>::Quantity(Quantity<IntList, T> const& num) : val(num.val) { }
 
-    template<typename IntList>
-    Quantity<IntList>& Quantity<IntList>::operator=(Quantity<IntList> const& num)
+    template<typename IntList, class T>
+    Quantity<IntList, T>& Quantity<IntList, T>::operator=(Quantity<IntList, T> const& num)
     {
         val = num.val;
 
         return *this;
     }
 
-    template<typename IntList>
-    double const& Quantity<IntList>::get_value() const
+    template<typename IntList, class T>
+    T const& Quantity<IntList, T>::get_value() const
     {
         return val;
     }
 
-    template<typename IntList>
-    double& Quantity<IntList>::get_value()
+    template<typename IntList, class T>
+    T& Quantity<IntList, T>::get_value()
     {
         return const_cast<double&>(const_cast<Quantity const*>(this)->get_value());
     }
 
-    template<typename IntList>
-    Quantity<IntList>& Quantity<IntList>::operator-=(Quantity const& rhs)
+    template<typename IntList, class T>
+    Quantity<IntList, T>& Quantity<IntList, T>::operator-=(Quantity<IntList, T> const& rhs)
     {
         val -= rhs.val;
 
         return *this;
     }
 
-    template<typename IntList>
-    Quantity<IntList>& Quantity<IntList>::operator+=(Quantity const& rhs)
+    template<typename IntList, class T>
+    Quantity<IntList, T>& Quantity<IntList, T>::operator+=(Quantity<IntList, T> const& rhs)
     {
         val += rhs.val;
 
         return *this;
     }
 
-    template<typename IntList>
-    Quantity<IntList>& Quantity<IntList>::operator*=(Quantity const& rhs)
+    template<typename IntList, class T>
+    Quantity<IntList, T>& Quantity<IntList, T>::operator*=(Quantity<IntList, T> const& rhs)
     {
         val *= rhs.val;
 
         return *this;
     }
 
-    template<typename IntList>
-    Quantity<IntList>& Quantity<IntList>::operator/=(Quantity const& rhs)
+    template<typename IntList, class T>
+    Quantity<IntList, T>& Quantity<IntList, T>::operator/=(Quantity<IntList, T> const& rhs)
     {
         val /= rhs.val;
 
         return *this;
     }
 
-    template<typename IntList>
-    bool operator<(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    bool operator<(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs)
     {
         return lhs.get_value() < rhs.get_value();
     }
 
-    template<typename IntList>
-    bool operator>(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    bool operator>(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs)
     {
         return lhs.get_value() > rhs.get_value();
     }
 
-    template<typename IntList>
-    bool operator==(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    bool operator==(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs)
     {
         return lhs.get_value() == rhs.get_value();
     }
 
-    template<typename IntList>
-    bool operator!=(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    bool operator!=(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs)
     {
         return lhs.get_value() != rhs.get_value();
     }
@@ -165,45 +165,45 @@ namespace PhysicalNumbers
         return lhs.get_value() <= rhs.get_value();
     }
 
-    template<typename IntList>
-    bool operator>=(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    bool operator>=(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs)
     {
         return lhs.get_value() >= rhs.get_value();
     }
 
-    template<typename IntList>
-    Quantity<IntList> operator-(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    Quantity<IntList, T> operator-(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs)
     {
-        return Quantity<IntList>(lhs) -= rhs;
+        return Quantity<IntList, T>(lhs) -= rhs;
     }
 
-    template<typename IntList>
-    Quantity<IntList> operator+(Quantity<IntList> const& lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    Quantity<IntList, T> operator+(Quantity<IntList, T> const& lhs, Quantity<IntList, T> const& rhs)
     {
-        return Quantity<IntList>(lhs) += rhs;
+        return Quantity<IntList, T>(lhs) += rhs;
     }
 
-    template<typename IntList1, typename IntList2>
-    Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Plus>> operator*(Quantity<IntList1> const& lhs, Quantity<IntList2> const& rhs)
+    template<typename IntList1, typename IntList2, class T>
+    Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Plus>, T> operator*(Quantity<IntList1, T> const& lhs, Quantity<IntList2, T> const& rhs)
     {
-        return Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Plus>>(lhs.get_value() * rhs.get_value());
+        return Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Plus>, T>(lhs.get_value() * rhs.get_value());
     }
 
-    template<typename IntList1, typename IntList2>
-    Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Minus>> operator/(Quantity<IntList1> const& lhs, Quantity<IntList2> const& rhs)
+    template<typename IntList1, typename IntList2, class T>
+    Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Minus>, T> operator/(Quantity<IntList1, T> const& lhs, Quantity<IntList2, T> const& rhs)
     {
         return Quantity<MetaProgramming::Zip<IntList1, IntList2, MetaProgramming::Minus>>(lhs.get_value() / rhs.get_value());
     }
 
-    template<typename IntList>
-    Quantity<IntList> operator*(double lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    Quantity<IntList, T> operator*(double lhs, Quantity<IntList, T> const& rhs)
     {
-        return Quantity<IntList>(lhs * rhs.get_value());
+        return Quantity<IntList, T>(lhs * rhs.get_value());
     }
 
-    template<typename IntList>
-    Quantity<MetaProgramming::Zip<MetaProgramming::Generate<MetaProgramming::Length<IntList>::value>, IntList, MetaProgramming::Minus>> operator/(double lhs, Quantity<IntList> const& rhs)
+    template<typename IntList, class T>
+    Quantity<MetaProgramming::Zip<MetaProgramming::Generate<MetaProgramming::Length<IntList>::value>, IntList, MetaProgramming::Minus>, T> operator/(double lhs, Quantity<IntList, T> const& rhs)
     {
-        return Quantity<MetaProgramming::Zip<MetaProgramming::Generate<MetaProgramming::Length<IntList>::value>, IntList, MetaProgramming::Minus>>(lhs / rhs.get_value());
+        return Quantity<MetaProgramming::Zip<MetaProgramming::Generate<MetaProgramming::Length<IntList>::value>, IntList, MetaProgramming::Minus>, T>(lhs / rhs.get_value());
     }
 }
